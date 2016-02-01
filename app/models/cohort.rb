247becce4d42a9688c6ff6a65703c8ba86d52ec3
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-01 15:25:30 -0500

    Validate cohort_number as integer
    Pass test 5 : "is invalid with a non-integer cohort number"
    
    Use `numericality` helper.

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,6 +1,6 @@
 class Cohort < ActiveRecord::Base
   attr_accessor :cohort_number, :start_date, :end_date
-  validates :cohort_number, presence: true
+  validates :cohort_number, presence: true, numericality: {only_integer: true, message: "must be an integer"}
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
