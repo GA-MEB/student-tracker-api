5c7e70f830fdfd48ae94461b2eb7eb769605313b
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-01-31 21:48:59 -0500

    Add columns to migration & accessors to model
    Pass test 1 : "is valid with cohort number, start date, and end date"

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,2 +1,3 @@
 class Cohort < ActiveRecord::Base
+  attr_accessor :cohort_number, :start_date, :end_date
 end
--- db/migrate/20160131173204_create_cohorts.rb
+++ db/migrate/20160131173204_create_cohorts.rb
@@ -1,6 +1,9 @@
 class CreateCohorts < ActiveRecord::Migration
   def change
     create_table :cohorts do |t|
+      t.integer :cohort_number
+      t.date :start_date
+      t.date :end_date
       t.timestamps null: false
     end
   end
