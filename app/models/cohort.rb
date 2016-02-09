62cb78596c221e83c0088619d40bac9036c8c51e
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-09 13:45:50 -0500

    Validate that start_date and end_date are dates
    Pass test 8 : "is invalid with an invalid start date or end date"
    
    Wrote a simple custom validation method for this.

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -7,4 +7,11 @@ class Cohort < ActiveRecord::Base
     message: "must be an integer greater than zero"
   }
   validates_presence_of :start_date, :end_date
+  validate :start_date_and_end_date_are_dates
+
+  private
+  def start_date_and_end_date_are_dates
+    errors.add(:start_date, "must be a date") unless start_date.is_a? Date
+    errors.add(:end_date, "must be a date") unless end_date.is_a? Date
+  end
 end
