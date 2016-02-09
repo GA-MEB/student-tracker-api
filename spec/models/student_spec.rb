83e07458f357f7e15aada4710c5c5b7a09fcccc9
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-09 14:42:24 -0500

    Add tests for Student to validate presence
    Should validate presence of:
    - given name
    - surname
    - student id number
    
    Leaving out one of these is invalid.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a given name, surname, and student id number" do
+    student = Student.new(
+      given_name: 'Charles',
+      surname: 'Xavier',
+      student_id_number: 10
+    )
+    expect(student).to be_valid
+  end
+  it "is invalid without a given name" do
+    no_given_name = Student.new(given_name: nil)
+    no_given_name.valid?
+    expect(no_given_name.errors[:given_name]).to include("can't be blank")
+  end
+  it "is invalid without a surname" do
+    no_surname = Student.new(surname: nil)
+    no_surname.valid?
+    expect(no_surname.errors[:surname]).to include("can't be blank")
+  end
+  it "is invalid without a student id number" do
+    no_student_id_number = Student.new(student_id_number: nil)
+    no_student_id_number.valid?
+    expect(no_student_id_number.errors[:student_id_number]).to include(
+      "can't be blank"
+    )
+  end
 end
