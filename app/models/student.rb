71fd3914a0ca99227327a3884b0984131afead22
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-24 00:35:04 -0500

    Validate that student_id_number is always >= 1
    Pass test : "is invalid with a zero or negative student id number"

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ActiveRecord::Base
   validates_presence_of :given_name, :surname, :student_id_number
   validates :student_id_number,
-    uniqueness: {message: "must be unique"}
+    uniqueness: {message: "must be unique"},
+    numericality: {
+      only_integer: true,
+      greater_than: 0,
+      message: "must be an integer greater than zero"
+    }
+
 end
