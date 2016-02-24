469e7590047f3a6ce87904b5f419a2aa0d0b7e6e
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-24 10:25:36 -0500

    Create controller (w/ spec) for cohorts
    Add standard methods to controller spec,
    but don't actually add the tests yet.

---
+++ spec/controllers/cohorts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe CohortsController, type: :controller do
+  describe "GET #index" do
+  end
+  describe "GET #show" do
+  end
+  describe "POST #create" do
+  end
+  describe "PATCH/PUT #update" do
+  end
+  describe "DELETE #destroy" do
+  end
+end
