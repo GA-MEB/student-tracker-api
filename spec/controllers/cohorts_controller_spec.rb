9b812fbb528e757015f5b50e4c82db6de0673ffe
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-24 14:34:18 -0500

    Add contexts, outline to cohorts controller
    Add valid and invalid paths for POST and PATCH/PUT
    Use comments as placeholders for future `it` blocks.

--- spec/controllers/cohorts_controller_spec.rb
+++ spec/controllers/cohorts_controller_spec.rb
@@ -2,13 +2,33 @@ require 'rails_helper'
 
 RSpec.describe CohortsController, type: :controller do
   describe "GET #index" do
+    # it "assigns an array of cohorts to @cohorts"
+    # it "renders @cohorts as a JSON response"
   end
   describe "GET #show" do
+    # it "assigns the requested cohort to @cohort"
+    # it "renders @cohort as a JSON response"
   end
   describe "POST #create" do
+    context "with valid attributes" do
+      # it "creates a new cohort in the database"
+      # it "renders a JSON response"
+    end
+    context "with invalid attributes" do
+      # it "is not successful"
+    end
   end
   describe "PATCH/PUT #update" do
+    context "with valid attributes" do
+      # it "updates the requested cohort"
+      # it "assigns the requested cohort to @cohort"
+      # it "renders @cohort as a JSON response"
+    end
+    context "with invalid attributes" do
+      # it "is not successful"
+    end
   end
   describe "DELETE #destroy" do
+    # it "destroys the requested cohort"
   end
 end
