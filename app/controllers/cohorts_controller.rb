b6eecb21acd41dc1db5a05006d6fbfef61e3b0ed
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-25 12:12:05 -0500

    Add #show to cohort controller
    Passes Controller Specs:
    - "GET #show is successful"
    - "GET #show renders a JSON response"
    
    Passes Request Specs:
    - "GET /cohorts/:id shows one cohort"

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,5 +1,8 @@
 class CohortsController < ApplicationController
   def index
-    render json: Cohort.all
+    render json: Cohort.all, root: 'cohorts'
+  end
+  def show
+    render json: Cohort.find(params[:id]), root: 'cohort'
   end
 end
