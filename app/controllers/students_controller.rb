b0fbb483f8a60696ee3aadae7aa6a395b9fbbd28
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-26 18:22:56 -0500

    Implement students#show
    Pass these Controller specs:
    - "GET #show is successful"
    - "GET #show renders a JSON response"
    
    Pass these Request specs:
    - "GET /students/:id shows one student"

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,7 @@ class StudentsController < ApplicationController
   def index
     render json: Student.all, root: 'students'
   end
+  def show
+    render json: Student.find(params[:id]), root: 'student'
+  end
 end
