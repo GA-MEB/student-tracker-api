fd9e928301f0c7d089b3d7d30380e999175fc471
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-02-27 20:07:13 -0500

    Implement students#create
    Pass these Controller specs:
    - "POST #create is successful"
    - "POST #create renders a JSON response"
    
    Pass these Request specs:
    - "POST /students with valid attributes creates a new student"
    - "POST /students with invalid attributes is not successful"

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
   def show
     render json: Student.find(params[:id]), root: 'student'
   end
+  def create
+    @student = Student.create(student_params)
+    if @student.save
+      render json: @student, status: :created, root: 'student'
+    else
+      render json: @student.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(
+      [:given_name, :surname, :student_id_number]
+    )
+  end
 end
