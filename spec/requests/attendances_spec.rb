d733d2d01f2da7306b89ff2b69aeabf13314a434
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-11 15:49:47 -0500

    Make request specs for attendances#index
    Add these Request specs:
    - "GET /students/:student_id/attendances with a valid student id
     gets all attendances for the specified student"
    - "GET /students/:student_id/attendances with an invalid student id
     is not successful"

--- spec/requests/attendances_spec.rb
+++ spec/requests/attendances_spec.rb
@@ -1,10 +1,62 @@
 require 'rails_helper'
 
+def attendances_params
+  [
+    { date: Date.parse('2016-03-10'), status: 'present' },
+    { date: Date.parse('2016-03-11'), status: 'present' },
+    { date: Date.parse('2016-03-12'), status: 'present' },
+    { date: Date.parse('2016-03-13'), status: 'tardy' },
+    { date: Date.parse('2016-03-14'), status: 'tardy' },
+    { date: Date.parse('2016-03-15'), status: 'left early' },
+    { date: Date.parse('2016-03-16'), status: 'absent' }
+  ]
+end
+
+def attendance
+  Attendance.last
+end
+
+def student_params
+  {
+    given_name: 'Bob',
+    surname: 'Marley',
+    student_id_number: 10
+  }
+end
+
+def student
+  Student.last
+end
+
 RSpec.describe "Attendances", type: :request do
-  describe "GET /attendances" do
-    it "works! (now write some real specs)" do
-      get attendances_index_path
-      expect(response).to have_http_status(200)
+  before(:all) do
+    Student.create!(student_params)
+    attendances_params.each do |params|
+      student.attendances << Attendance.create(params)
+    end
+  end
+  after(:all) do
+    Student.delete_all
+    Attendance.delete_all
+  end
+
+  describe "GET /students/:student_id/attendances" do
+    context "with a valid student id" do
+      it "retrieves all attendances associated with a given student" do
+        get "/students/#{student.id}/attendances"
+        expect(response).to be_success
+
+        attendances_response = JSON.parse(response.body)
+        expect(attendances_response['attendances'].length
+          ).to eq(student.attendances.count)
+      end
+    end
+    context "with an invalid student id" do
+      it "is unsuccessful" do
+        invalid_student_id = 0
+        get "/students/#{invalid_student_id}/attendances"
+        expect(response).to_not be_success
+      end
     end
   end
 end
