9a397dee0894eddcc8fe54f9e6038c70a7d33d4d
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-14 16:03:38 -0400

    Refactor attendances#index and attendances#show
    Replace the controller's before_action with a basic helper method.
    This makes the controller actions a bit more semantic.

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,16 +1,16 @@
 class AttendancesController < ApplicationController
-  before_action :set_student
 
   def index
-    render json: @student.attendances, root: 'attendances'
+    render json: attendances, root: 'attendances'
   end
 
   def show
-    render json: @student.attendances.find(params[:id]), root: 'attendance'
+    render json: attendances.find(params[:id]), root: 'attendance'
   end
 
   private
-  def set_student
-    @student = Student.find(params[:student_id])
+  def attendances
+    Student.find(params[:student_id]).attendances
   end
+  
 end
