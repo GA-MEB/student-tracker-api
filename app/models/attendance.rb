e63688670c165cb39464ea69d4e8da3841d10400
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-15 22:37:38 -0400

    Validate that `date` is not a future date
    Create a private helper, `date_is_not_future`, to use as a custom validator.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,6 +2,14 @@ class Attendance < ActiveRecord::Base
   enum status: [:present, :absent, :tardy, :left_early]
 
   validates_presence_of :date, :status
+  validate :date_is_not_future
 
   belongs_to :student, inverse_of: :attendances
+
+  private
+  def date_is_not_future
+    if date && date > Date.today
+      errors.add(:date, "must not be a future date")
+    end
+  end
 end
