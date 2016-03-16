7259e98110ae6347dc54a1a5cec83f2bc6f9ce4c
Matt Brendzel <matt.brendzel@generalassemb.ly>
2016-03-15 21:26:39 -0400

    Fix CreateAttendances migration
    Without realizing it, I caused my test and development dbs to get out of step.
    I did this by making my migrations, running tests based on those migrations,
     and then rolling them back on development **but NOT on test**.
    As a result, my tests were all passing, and the appropriate columns existed in
     the database, but they weren't specified in the migrations, so if I had
     deployed the app, it wouldn't have worked on the remote server.
    When I ran `bundle exec rake db:drop db:create db:migrate RAILS_ENV=test`,
     I was able to get my tests to fail again.
    At that point, all I needed to do was roll back to the migration, fix it,
     and run `rake db:migrate`; then, do the same for the test database.
    Lesson learned...

--- db/migrate/20160312230806_create_attendances.rb
+++ db/migrate/20160312230806_create_attendances.rb
@@ -1,6 +1,8 @@
 class CreateAttendances < ActiveRecord::Migration
   def change
     create_table :attendances do |t|
+      t.date :date
+      t.integer :status
       t.timestamps null: false
     end
   end
